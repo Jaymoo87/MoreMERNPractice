39462c70be7dbf2fdaca0a52dec2d65685587836
Justin Murrah <jdmurrah@gmail.com>
2023-02-07 12:19:16 -0600

    create working bout to display it

--- server/index.js
+++ server/index.js
@@ -6,14 +6,12 @@ import studentRouter from "./routes/student.js";
 
 const app = express();
 
-app.use("/students", studentRouter);
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
-
 app.use(cors());
+app.use("/students", studentRouter);
 
 const CONNECTION_URL = "mongodb+srv://admin:Rowan2014@jaymoomongo.rs7nh.mongodb.net/?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.set("strictQuery", false);
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createStudent, getStudents } from "../controllers/student.js";
+import student from "../models/student-model.js";
 
 const studentRouter = express.Router();
 
